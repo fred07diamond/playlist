93306c105d121cd2f109a7f9dfd235477cbdd873
Fred Diamond <freddiamond09@gmail.com>
2019-01-10 07:03:16 +0000

    finished all JS; including arays, foreach loops, functions, jquery. Messed with HTML and CSS, but didnt add anything.

--- README.md
+++ README.md
@@ -8,43 +8,43 @@
 ### Set-Up
 - [x] Go to the repository at https://github.com/ScriptEdcurriculum/playlist
 - [x] Fork this repository to your github account and clone it to a new workspace
-- [ ] Add, commit, and push your changes
-- [ ] Make your site live on gh-pages
+- [x] Add, commit, and push your changes
+- [x] Make your site live on gh-pages
 
 ### JS
-- [ ] Change the songs in the songs array
-- [ ] Update the song array in your project to have at least 4 songs
-- [ ] Create four new arrays to store
-- [ ] images_urls, artists, song lengths, and links
+- [x] Change the songs in the songs array
+- [x] Update the song array in your project to have at least 4 songs
+- [x] Create four new arrays to store
+- [x] images_urls, artists, song lengths, and links
 ### html / css
-- [ ] Customize your CSS or add another column
+- [x] Customize your CSS or add another column
 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Day 2
 ### JS
-- [ ] Use forEach Loop to display the data from each of your array's in the correct div
-- [ ] Update the code to append and HTML Elements (img, p, a tags) using jQuery. 
+- [x] Use forEach Loop to display the data from each of your array's in the correct div
+- [x] Update the code to append and HTML Elements (img, p, a tags) using jQuery. 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 
 ## Day 3
 ### JS
-- [ ] A working click handler
-- [ ] Retrieve user input
-- [ ] Push items to your song array
-- [ ] Display the result to the screen
+- [x] A working click handler
+- [x] Retrieve user input
+- [x] Push items to your song array
+- [x] Display the result to the screen
 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Extensions
-- [ ] Update the HTML and CSS to reflect your own style.
-- [ ] Add more songs to your list and song information to your list
-- [ ] Add a delete button to delete songs on your list
-- [ ] Use local storage to save your songs locally
+- [x] Update the HTML and CSS to reflect your own style.
+- [x] Add more songs to your list and song information to your list
+- [x] Add a delete button to delete songs on your list
+- [x] Use local storage to save your songs locally
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,34 +1,99 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = [
+    'Speecheless',
+    'Tequila', 
+    'Meant To Be',
+    'She Got The Best of Me',
+    'Lose It',
+    'Best Shot'
+    ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var artists = [
+    'Dan and Shay',
+    'Dan and Shay',
+    'Florida Georgia Line',
+    'Luke Combs',
+    'Kane Brown',
+    'Jimmie Allen',
+    ];
 
+var images = [
+    'http://bit.ly/2H4tTy2',
+    'http://bit.ly/2SKpXDP',
+    'http://bit.ly/2M1TfLF',
+    'http://bit.ly/2H3VEa2',
+    'http://bit.ly/2CYOW0H',
+    'http://bit.ly/2FoErFx',
+    ];
 
+var lengths = [
+    '3:33',
+    '3:16',
+    '2:43',
+    '3:03',
+    '3:01',
+    '3:16',
+    ];
+var links = [
+    'http://bit.ly/2FjDEqy',
+    'http://bit.ly/2QxOF8x',
+    'http://bit.ly/2AEVVum',
+    'http://bit.ly/2H53VdB',
+    'http://bit.ly/2Fm1L7Q',
+    ];
 
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
+
+
+function displaySongInfo(){
+    images.forEach(function(image) {
+        $("#images").append("<img src='" + image + "'>");   
+    });
+
+    songs.forEach(function(song) {
+        $("#songs").append("<p>" + song + "</p>"); 
+    });
+
+    artists.forEach(function(artist) {
+        $("#artists").append("<p> By: " + artist + "</p>");   
+    });
+
+    lengths.forEach(function(length) {
+        $("#lengths").append("<p>" + length + "</p>"); 
+    });
+
+    links.forEach(function(link) {
+        $("#links").append("<a href='" + link + "'> Listen </a>"); 
+    });
 }
 
 function emptySongInfo(){
+    $("#images").empty();
     $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-
-
-}
-
-
-function addSongInfo(){
-    // BELOW write the code to add new items to each of the arrays.
-
-
+    $("#artists").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 }
 
 $("#add").click(function() {
-    emptySongInfo();
-    addSongInfo();
+    var songName = $("#song").val();
+    var imageUrl = $("#image").val();
+    var artistName = $("#artist").val();
+    var songLength = $("#length").val();
+    var link = $("#links").val();
+
+    songs.push(songName);
+    artists.push(artistName);
+    images.push(imageUrl);
+    lengths.push(songLength);
+    links.push(link);
+
+    emptySongInfo()
     displaySongInfo();
 });
 
